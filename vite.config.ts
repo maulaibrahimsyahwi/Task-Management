33fc0bc178cbea564a828f124de77955ce28b13e
Maula Ibrahim Syahhwi <boimmuhammad8@gmail.com>
2025-12-22 00:04:59 +0700

    improve

--- vite.config.ts
+++ vite.config.ts
@@ -4,32 +4,11 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   build: {
+    chunkSizeWarningLimit: 1600,
     rollupOptions: {
       output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            if (id.includes("firebase")) {
-              return "firebase";
-            }
-            if (
-              id.includes("react") ||
-              id.includes("react-dom") ||
-              id.includes("react-router-dom")
-            ) {
-              return "react-vendor";
-            }
-            if (
-              id.includes("styled-components") ||
-              id.includes("@hello-pangea/dnd") ||
-              id.includes("lucide-react")
-            ) {
-              return "ui-vendor";
-            }
-            return "vendor";
-          }
-        },
+        manualChunks: undefined,
       },
     },
-    chunkSizeWarningLimit: 1000,
   },
 });
